2f112f630452245d1fb3b1424be4f48843490a42
kungminlin <lin.kungmin@gmail.com>
2018-04-21 13:36:54 -0700

    added form processing
    added form processing

--- client.js
+++ client.js
@@ -7,6 +7,22 @@
 
 console.log("test");
 
+function processForm() {
+	var name = document.forms["assignmentForm"]["name"].value;
+	var desc = document.forms["assignmentForm"]["desc"].value;
+	var time = document.forms["assignmentForm"]["time"].value;
+	if (name=="") {
+		alert("Name must be filled out");
+		return false;
+	} else if (time==0) {
+		alert("Invalid time");
+		return false;
+	} else {
+		addAssignment(name, desc, time);
+		return true;
+	}
+}
+
 $('#start-button').click(function() {
 	console.log("started timer");
 	startTimer(10);
