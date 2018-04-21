44178d9df6069853b78c0bb1ea628972da8e914e
kungminlin <lin.kungmin@gmail.com>
2018-04-21 09:58:37 -0700

    added timer
    added timer

--- background.js
+++ background.js
@@ -1,1 +1,35 @@
-console.log("test");
+// chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
+// 	var url = tabs[0].url;
+// 	console.log(url);x
+//});
+
+// var assignments = [];
+
+// function addAssignment(assignment) {
+// 	assignments.push(assignment);
+// 	for (var i=0; i<assignments.length; i++) {
+
+// 	}
+// }
+
+// function removeAssignment(id) {
+// 	assignments.splice(id, 1);
+// }
+	
+// function startTimer(assignment) {
+// 	setTimeout(function() {
+// 		$("#assignmentTitle").html(assignment.name);
+// 	}, assignment.time*1000);
+// }
+
+function startTimer(time) {
+	var counter = time;
+	var stopwatch = setInterval(function() {
+		counter--;
+		$("#timer").html(counter);
+	}, 1000);
+	$("#timer").html("")
+	setTimeout(function() {
+		alert("times up!");
+	})
+}
