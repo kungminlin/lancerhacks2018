9c6c5cf6aa5df35cafb8276a0dd5f2decebb0cd3
kungminlin <33560439+kungminlin@users.noreply.github.com>
2018-04-24 21:32:30 -0700

    remove assignments
    fixed removing assignments mechanism

--- background.js
+++ background.js
@@ -10,11 +10,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		"from the extensions");
 	if (request.add_assignment != null) {
 		var assignment = request.add_assignment
-		addAssignment(assignment.name, assignment.desc, assignment.time);
+		addAssignment(assignment.id, assignment.name, assignment.desc, assignment.time);
 		sendResponse({farewell: "assignment added"});
-	} else if (request.start_assignment != null && Number.isInteger(request.start_assignment.time) && request.start_assignment.name != "") {
+	} else if (request.start_assignment != null && Number.isInteger(request.start_assignment.time) && Number.isInteger(request.start_assignment.id) && request.start_assignment.name != "") {
 		var assignment = request.start_assignment;
-		startStudy(assignment.time, assignment.name);
+		startStudy(assignment.id, assignment.name, assignment.time);
 	} else if (request.add_website.name != "") {
 		var website = request.add_website;
 		addURL(website.name, website.link);
@@ -22,18 +22,15 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	}
 });
 
-function addAssignment(name, desc, time) {
+function addAssignment(id, name, desc, time) {
 	chrome.storage.local.get(['assignments'], function(assignments) {
-		assignments.assignments.push({name: name, desc: desc, time: time});
+		assignments.assignments.push({id: id, name: name, desc: desc, time: time});
 		chrome.storage.local.set({'assignments': assignments.assignments});
 	});
-	chrome.tabs.query({active: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {update_assignment: true});
-	});
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		console.log("test");
-	 	chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {});
-	});
+	// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	// 	console.log("test");
+	//  	chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {});
+	// });
 }
 
 function addURL(name, link) {
@@ -72,8 +69,45 @@ if (!window.Notification) {
 	});
 }
 
-function startStudy(time, assignmentName) {
-	startTimer(time, assignmentName + " completed!", "Please click on this notification in order to choose your next step.");
+function startStudy(assignmentId, assignmentName, time) {
+	var counter = time;
+	if (blockedSites.length > 0) {
+		console.log(blockedSites);
+		chrome.webRequest.onBeforeRequest.addListener(
+		    blockURL,
+		    {urls: blockedSites},
+		    ["blocking"]
+		);
+		
+	}
+	var stopwatch = setInterval(function() {
+		chrome.storage.local.set({'currTime': counter});
+		var timeStr = pad(Math.floor(counter/3600)%60,2) + " : " + pad(Math.floor(counter/60)%60,2) + " : " + pad(counter%60,2);
+		console.log(timeStr);
+		chrome.browserAction.setTitle({title: timeStr});
+		if (counter <= 0) {
+			chrome.webRequest.onBeforeRequest.removeListener(blockURL);
+			chrome.storage.local.get(['assignments'], function(assignments) {
+				var assignments = assignments.assignments;
+				for (var i=0; i<assignments.length; i++) if (assignments[i].id == assignmentId) assignments.splice(i, 1);
+				chrome.storage.local.set({assignments: assignments});
+			});
+			clearInterval(stopwatch);
+			var notify;
+			if (Notification.permission === 'default') {
+				alert('Notification not enabled');
+			} else {
+				notify = new Notification(assignmentName + " completed!", {
+					body: "You have completed the assignment! You can now choose to take a break or continue studying."
+				});
+				notify.onclick = function() {
+					chrome.tabs.create({url:"assignments.html"});
+				}
+			}
+		}
+		counter--;
+	}, 1000);
+	console.log("done");
 }
 
 function startBreak(time) {
--- client.js
+++ client.js
@@ -1,13 +1,13 @@
 var timing = false;
 
+updateAssignments();
+
 $(document).ready(function() {
 	$(document).on("click", "li.assignment", function() {
 		if (!timing) {
 			console.log(parseInt($(this).children('.assignment_time')[0].innerText));
-			chrome.runtime.sendMessage({start_assignment: {name: $(this).children('.assignment_name')[0].innerText, time: parseInt($(this).children('.assignment_time')[0].innerText)}});
-		} else {
-			//alert("still timing!");
-		}
+			chrome.runtime.sendMessage({start_assignment: {id: parseInt($(this).attr('data-assignmentId')), name: $(this).children('.assignment_name')[0].innerText, time: parseInt($(this).children('.assignment_time')[0].innerText)}});
+		} 
 	});
 });
 
@@ -32,18 +32,21 @@ var stopwatch = setInterval(function() {
 	chrome.storage.local.get(['currTime'], function(time) {
 		var timerStr = pad(Math.floor(time.currTime/3600)%60,2) + " : " + pad(Math.floor(time.currTime/60)%60,2) + " : " + pad(time.currTime%60,2);
 		$('#timer').html(timerStr);
-		if (time.currTime <= 0) timing = false;
+		if (time.currTime <= 0) {
+			timing = false;
+		}
 		else timing = true;
 	});
 }, 100);
 
-chrome.storage.local.get(['assignments'], function(assignments) {
-	for (var i=0; i<assignments.assignments.length; i++) {
-		console.log(assignments.assignments[i].name + ": " + assignments.assignments[i].time);
-		$("#assignment_list").append("<li class='assignment'><p class='assignment_name'>" + assignments.assignments[i].name + "</p><p class='assignment_time'>" + assignments.assignments[i].time/60 + " min.</p></li>");
-	}
-	console.log("assignment updated");
-}); 
+function updateAssignments() {
+	chrome.storage.local.get(['assignments'], function(assignments) {
+		$("#assignment_list").empty();
+		for (var i=0; i<assignments.assignments.length; i++) {
+			$("#assignment_list").append("<li data-assignmentId='" + assignments.assignments[i].id + "' class='assignment'><p class='assignment_name'>" + assignments.assignments[i].name + "</p><p class='assignment_time'>" + assignments.assignments[i].time/60 + " min.</p></li>");
+		}
+	}); 
+}
 	
 chrome.storage.sync.get(['blacklist'], function(blacklist) {
 	for (var i=0; i<blacklist.blacklist.length; i++) {
@@ -73,8 +76,9 @@ function processAssignment() {
 		alert("The time must be a value above 10 minutes");
 		return false;
 	} else {
-		$("#assignment_list").append("<li class='assignment'><p class='assignment_name'>" + name + "</p><p class='assignment_time'>" + time + " min.</p></li>");
-		chrome.runtime.sendMessage({add_assignment: {name: name, desc: desc, time: time*60}});
+		var id = (new Date()).getTime();
+		$("#assignment_list").append("<li data-assignmentId='" + id + "' class='assignment'><p class='assignment_name'>" + name + "</p><p class='assignment_time'>" + time + " min.</p></li>");
+		chrome.runtime.sendMessage({add_assignment: {id: id, name: name, desc: desc, time: time*60}});
 		return true;
 	}
 }
@@ -97,11 +101,5 @@ function processLink() {
 // 	console.log(sender.tab ?
 // 		"from a content script: " + sender.tab.url :
 // 		"from the extension");
-// 	console.log("assignment updated");
-// 	updateAssignments();
-// });
-
-// $('#start-button').click(function() {
-// 	console.log("started timer");
-// 	startTimer(10);
+// 	if (request.greeting == "hello") console.log("success!");
 // });
